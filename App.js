f7df4a317455d99a87c11143b28b235802c6e5c7
Tchasinga jacques <tchasingajacques@gmail.com>
2024-08-05 08:45:56 -0700

    new side

--- App.js
+++ App.js
@@ -21,7 +21,7 @@ const HomeScreen = ({ navigation }) => {
     } catch (error) {
       console.error('Error fetching data:', error);
     } finally {
-      setLoading(false); // this  be Set loading to   
+      setLoading(false); // this  be Set loading    
     }
   };
 
